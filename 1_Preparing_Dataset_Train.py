d7c92d3d76dfa0f3901bfadfdb3c3ae47565d207
Gaetano Salvatore Falco <gaetanosalvatore.falco@protonmail.com>
2023-01-15 19:06:02 +0100

    Set correct path to load audio

--- 1_Preparing_Dataset_Train.py
+++ 1_Preparing_Dataset_Train.py
@@ -17,12 +17,13 @@ folder_path = '../../datasets/dsl_data/'
 ###############
 def process_file(file_path):
     file_path_exists = df[df["path"] == file_path].shape[0] > 0 #flag
+    # print(file_path)
+    # print(file_path_exists)
     if file_path_exists:
         new_sr=16000
         # print(file_path)
 
         # identifier care
-
         identifier = df.loc[df["path"] == file_path, "Id"].values[0]
         identifier = str(int(identifier))
         # print("\n "+identifier)
@@ -35,77 +36,23 @@ def process_file(file_path):
 
         new_file_path = os.path.join(new_folder_path, identifier + "_" + label + '.wav')
 
-        y, sr = librosa.load(file_path)
+        # print(file_path)
+        y, sr = librosa.load('../../datasets/'+file_path)
+        # print("here")
+        # print(y)
         y_truncated = librosa.effects.trim(y, top_db=50, frame_length=2048, hop_length=512, ref=np.max)[0]
-
+        # print(y_truncated)
         y_truncated = librosa.resample(y_truncated, orig_sr=sr, target_sr=new_sr)
 
-        y_truncated = y_truncated[:int(4*new_sr)]
+        y_truncated = y_truncated[:int(4*new_sr)] #if longer
 
         target_length = 4 * new_sr
-        y_truncated = librosa.util.fix_length(data=y_truncated, size=target_length) #padding
+        y_truncated = librosa.util.fix_length(data=y_truncated, size=target_length) #padding, if shorter
 
         sf.write(new_file_path, y_truncated, new_sr, 'PCM_16')
 
-###############
-
 if not os.path.isdir(new_folder_path):
     os.makedirs(new_folder_path) #hoping to have write permissions set
-
-#this code will use only a thread ... and i have an AMD 5800x3d, so duck this code, infinire powaaaaaaaaaaaaaaa!
-# for dirpath, dirnames, filenames in os.walk(folder_path): #all sub folders
-#     # dirpath = dirpath[12:]
-#     # dirpath[14] = dirpath[8]
-#     # print(dirpath)
-#     dirpath = dirpath.replace("\\", "/")
-#     dirpath = dirpath[dirpath.index("/")+1:]
-
-#     # print(dirpath)
-#     for filename in filenames:
-#         file_path = os.path.join(dirpath, filename)
-#         # file_path = file_path[2:]
-#         file_path = file_path.replace("\\", "/")
-#         print(file_path)
-#         file_path_exists = df[df["path"] == file_path].shape[0] > 0 #flag
-#         # print(file_path)
-#         # print(df[df["path"] == file_path].shape[0]> 0)
-#         # print(df["path"]== file_path)
-#         # print(df.loc[df["path"] == file_path, "Id"] > 0)
-#         if file_path_exists:
-#             new_sr=16000
-#             print(file_path)
-
-#             # identifier care
-            
-#             identifier = df.loc[df["path"] == file_path, "Id"].values[0]
-#             identifier = str(int(identifier))
-            
-#             # label constructor
-
-#             action = df.loc[df["path"] == file_path, "action"].values[0]
-#             object = df.loc[df["path"] == file_path, "object"].values[0]
-#             label  = action + object
-
-
-#             new_file_path = os.path.join(new_folder_path, identifier + "_" + label + '.wav')
-
-#             y, sr = librosa.load(file_path)
-#             y_truncated = librosa.effects.trim(y, top_db=50, frame_length=2048, hop_length=512, ref=np.max)[0]
-
-#             y_truncated = librosa.resample(y_truncated, orig_sr=sr, target_sr=new_sr)
-
-#             y_truncated = y_truncated[:int(4*new_sr)]
-
-#             target_length = 4 * new_sr
-#             y_truncated = librosa.util.fix_length(data=y_truncated, size=target_length) #padding
-
-#             # y_truncated, sr = librosa.load(y_truncated, sr=sr, duration=4.0)
-#             # Save truncated audio
-#             # librosa.output.write_wav(new_file_path, y_truncated, sr)
-#             sf.write(new_file_path, y_truncated, new_sr, 'PCM_16')
-
-
-#             # shutil.copy(file_path, new_file_path)
 
 with ThreadPoolExecutor(max_workers=4) as executor: #multi-threaded beast :)
     for dirpath, dirnames, filenames in os.walk(folder_path):
@@ -117,6 +64,8 @@ with ThreadPoolExecutor(max_workers=4) as executor: #multi-threaded beast :)
         for filename in filenames:
             file_path = os.path.join(dirpath, filename)
             file_path = file_path.replace("\\", "/")
+            # print(file_path)
+            # process_file(file_path)
             executor.submit(process_file, file_path)
 
 # print(df)
--- 2_Preparing_Dataset_Evaluation.py
+++ 2_Preparing_Dataset_Evaluation.py
@@ -19,6 +19,7 @@ if not os.path.isdir(new_folder_path):
 
 def process_file(file_path):
     file_path_exists = df[df["path"] == file_path].shape[0] > 0 #flag
+    # print(file_path_exists)
     if file_path_exists:
         new_sr=16000
         # print(file_path)
@@ -27,7 +28,7 @@ def process_file(file_path):
 
         identifier = df.loc[df["path"] == file_path, "Id"].values[0]
         identifier = str(int(identifier))
-        # print("\n "+identifier)
+        # print("\n " + identifier)
 
         # label constructor
 
@@ -37,8 +38,8 @@ def process_file(file_path):
 
         new_file_path = os.path.join(new_folder_path, identifier + '.wav')
 
-        y, sr = librosa.load(file_path)
-        y_truncated = librosa.effects.trim(y, top_db=50, frame_length=2048, hop_length=512, ref=np.max)[0]
+        y, sr = librosa.load('../../datasets/'+file_path)
+        y_truncated = librosa.effects.trim(y, top_db=10, frame_length=2048, hop_length=512, ref=np.max)[0]
 
         y_truncated = librosa.resample(y_truncated, orig_sr=sr, target_sr=new_sr)
 
@@ -46,7 +47,7 @@ def process_file(file_path):
 
         target_length = 4 * new_sr
         y_truncated = librosa.util.fix_length(data=y_truncated, size=target_length) #padding
-
+        # print(new_file_path)
         sf.write(new_file_path, y_truncated, new_sr, 'PCM_16')
 
 
@@ -54,41 +55,14 @@ with ThreadPoolExecutor(max_workers=4) as executor: #multi-threaded beast :)
     for dirpath, dirnames, filenames in os.walk(folder_path):
         dirpath = dirpath.replace("\\", "/")
         dirpath = dirpath[dirpath.index("/")+1:]
+        dirpath = dirpath[dirpath.index("/")+1:]
+        dirpath = dirpath[dirpath.index("/")+1:]
+        # print(dirpath)
         for filename in filenames:
             file_path = os.path.join(dirpath, filename)
             file_path = file_path.replace("\\", "/")
+            # print(file_path)
             executor.submit(process_file, file_path)
 
 # print(df)
 print("Execution ended")
-
-# for dirpath, dirnames, filenames in os.walk(folder_path): #all sub folders
-#     for filename in filenames:
-#         file_path = os.path.join(dirpath, filename)
-#         file_path = file_path[2:]
-        
-#         file_path_exists = df[df["path"] == file_path].shape[0] > 0 #flag
-#         # print(file_path)
-#         if file_path_exists:
-#             new_sr=16000
-            
-            # print(file_path)
-            # identifier = df.loc[df["path"] == file_path, "Id"].values[0]
-            # identifier = str(int(identifier))
-            # new_file_path = os.path.join(new_folder_path, identifier + '.wav')
-
-            # y, sr = librosa.load(file_path)
-            # y_truncated = librosa.effects.trim(y, top_db=20, frame_length=2048, hop_length=512, ref=np.max)[0]
-
-            # # y_truncated, sr = librosa.load(y_truncated, sr=sr, duration=4.0)
-            # y_truncated = librosa.resample(y_truncated, orig_sr=sr, target_sr=new_sr)
-            # y_truncated = y_truncated[:int(4*new_sr)]
-
-            # target_length = 4 * 16000
-            # y_truncated = librosa.util.fix_length(data=y_truncated, size=target_length) #padding
-
-            # sf.write(new_file_path, y_truncated, new_sr, 'PCM_16')
-
-            # shutil.copy(file_path, new_file_path)
-
-# print("Execution ended")
