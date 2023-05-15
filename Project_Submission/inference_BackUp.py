330ec2c27f087a7ab99006c5afe7da1e8c36fdb2
Gulisidan <gulisidan.kuerxi@protonmail.com>
2023-05-15 12:19:38 +0200

    Project end
    Ended all parts of the system architecture, tested and working on local pc

--- Project_Submission/inference_BackUp.py
+++ Project_Submission/inference_BackUp.py
@@ -16,13 +16,9 @@ from datetime import datetime
 import argparse as ap
 import pandas as pd
 import random
-import noisereduce as nr
-from scipy.io.wavfile import write
-from scipy.io import wavfile
 import paho.mqtt.client as mqtt
-
-import uuid
-mac_address = hex(uuid.getnode())
+import noisereduce as nr
+from scipy.io import wavfile
 
 os.chdir('../') #DELETE BEFORE SEND TO PROFESSOR
 
@@ -48,7 +44,7 @@ parser.add_argument('--downsampling_rate', default=8000, type=int, help="Resolut
 parser.add_argument('--device', default=0, type=int, help="Default device is 0, change for others")
 
 
-parser.add_argument('--output_directory', default='./',type=str, help='Used to specify output folder')
+parser.add_argument('--output_directory', default='.',type=str, help='Used to specify output folder')
 
 
 args = parser.parse_args(['--device','31','--resolution','8000' ])
@@ -56,9 +52,9 @@ args = parser.parse_args(['--device','31','--resolution','8000' ])
 
 blocksize = 4 * args.resolution
 LABELS = ['change languagenone', 'activatemusic', 'deactivatelights', 'increasevolume', 'decreasevolume', 'increaseheat', 'decreaseheat', 'nannan']
-
 print(LABELS)
 
+""" Necessary preprocessing args """
 frame_length_in_s = 0.04#0.032*2 # /2 for resnet18
 frame_step_in_s  = frame_length_in_s#frame_length_in_s
 
@@ -133,7 +129,6 @@ def get_spectrogram(indata, frame_length_in_s, frame_step_in_s):
 
     return spectrogram
 
-state = False
 
 def send_prediction_as_mqtt(predicted_label):
     # f'./{args.output_directory}/{timestamp}.wav'
@@ -144,33 +139,25 @@ def send_prediction_as_mqtt(predicted_label):
     #print("predicted label:",predicted_label)
     #print("max:",predicted_label.max())
     if predicted_label.max() > 0.6:
-        print(predicted_label.max()*100,"% confidence, sending label to mqtt")
+        print(predicted_label.max(),"% confidence, sending label to mqtt")
         index = ( np.where(predicted_label == predicted_label.max() )  )
         index = index[0][0]
         print("The predicted label is",LABELS[index])
-        client.publish(mqtt_topic, mac_address+'_'+str(index))
+        client.publish(mqtt_topic, int(index))
         
     else:
         print("Low confidence, sending noise to mqtt")
-        client.publish(mqtt_topic, mac_address+'_'+'7' )
+        client.publish(mqtt_topic, 7 )
     
     #print("index",index)
     #print("label",LABELS[index])
     print()
     
-def prediction_on_indata(filename):
-    
-    print("Prediction for",filename)
+
+def prediction_on_indata(indata):
     frame_length_in_s = 0.04
     frame_step_in_s   = frame_length_in_s
-    audio_binary = tf.io.read_file(filename)
-    audio, sampling_rate = tf.audio.decode_wav(audio_binary)
-    audio = tf.squeeze(audio, axis=-1) #all our audio are mono, drop extra axis
-    
-    #frame_length_in_s = 0.04
-    #frame_step_in_s   = frame_length_in_s
-    #global state
-    #audio = get_audio_from_numpy(indata)
+    audio = get_audio_from_numpy(indata)
     
     frame_length = int(frame_length_in_s * args.resolution)
     frame_step = int(frame_step_in_s * args.resolution)
@@ -193,19 +180,17 @@ def prediction_on_indata(filename):
     interpreter.invoke()
     output = interpreter.get_tensor(output_details[0]['index'])
 
-    print("change languagenone",output[0][0]*100,"%")
-    print("activatemusic",output[0][1]*100,"%")
-    print("deactivatelights",output[0][2]*100,"%")
-    print("increasevolume",output[0][3]*100,"%")
-    print("decreasevolume",output[0][4]*100,"%")
-    print("increaseheat",output[0][5]*100,"%")
-    print("decreaseheat",output[0][6]*100,"%")
+    #print("change languagenone",output[0][0]*100,"%")
+    #print("activatemusic",output[0][1]*100,"%")
+    #print("deactivatelights",output[0][2]*100,"%")
+    #print("increasevolume",output[0][3]*100,"%")
+    #print("decreasevolume",output[0][4]*100,"%")
+    #print("increaseheat",output[0][5]*100,"%")
+    #print("decreaseheat",output[0][6]*100,"%")
     #print("nannan",output[0][7]*100,"%")
     
     send_prediction_as_mqtt(output[0])
-    
-    return state
-
+    return
 
 values = sd.query_devices()
 device = 0
@@ -214,46 +199,23 @@ for value in values:
     if value['name'] == 'default':
         device = value['index']
 
-def callback2(indata, frames, callback_time, status):
-    timestamp = time()
-    global state
-    global mac_address
-    #if is_silence(indata) == 0 :
-        #calculate next step of FSM!
-    prediction_on_indata(indata)
-    print("Elapsed time: ",time()-timestamp)
-
 def callback(indata, frames, callback_time, status):
-    global identifier
-    global label
-    
-    # reduce noise in the audio data
-    
-    # write the audio data to file
-    write(f'./temp.wav', args.resolution, indata)
-    filesize_in_bytes = os.path.getsize(f'./temp.wav')
-    filesize_in_kb = filesize_in_bytes / 1024
-    print(f'Size: {filesize_in_kb:.2f}KB')
-    
-    
-    rate,data = wavfile.read(f'./temp.wav')
-    reduced_noise = nr.reduce_noise(y=data, sr=rate)
-    os.remove(f'./temp.wav')
-    wavfile.write(f'./temp.wav',rate,reduced_noise)
-    prediction_on_indata(f'./temp.wav')
+    timestamp = time()
 
-print(LABELS)
+    indata = indata.squeeze()
+    #print(indata.shape)
+    indata = nr.reduce_noise(y=indata, sr=32000) #sr=indata.shape[0])
+    #print(data.shape)
 
-def main_test():
-    with sd.InputStream(device=device, channels=1, dtype='int16', samplerate=args.resolution, blocksize=blocksize, callback=callback):
-            print("") # to print a new line, improving readability in the terminal
+    prediction_on_indata(indata)
+    
+    print("Elapsed time: ",time()-timestamp)
+    print()
 
 def main():
-
-    #print(LABELS)
     while True:
         with sd.InputStream(device=device, channels=1, dtype='int16', samplerate=args.resolution, blocksize=blocksize, callback=callback):
-            print("Talk, i am hearing") # to print a new line, improving readability in the terminal
+            print("TALK NOW OR SHUT UP")
 
 if __name__ == '__main__':
     main()
